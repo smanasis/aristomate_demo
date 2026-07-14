a554c5e7c3d5c161bc1134bf1bd69166104bab6a
smanasis <solonmanasis@gmail.com>
2026-07-14 12:29:15 +0300

    f around

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,74 @@
 <script>
-	import { universisFetch } from '$lib/universis_fetch';
+    import { universisFetch } from '$lib/universis_fetch';
+
+    let courses = [];
+
+    async function getGrades() {
+        const result = await universisFetch("students/me/courses");
+        courses = result.value;
+
+        console.log(courses);
+    }
+
+    let section = "grades";
+
 </script>
 
 
-  <ion-card>
-	<ion-card-header>
-		<ion-card-subtitle>Great success!!</ion-card-subtitle>
-		<ion-card-title>Welcome to your app!</ion-card-title>
-	</ion-card-header>
+<ion-header>
+    <ion-toolbar>
+        <ion-title>Universis</ion-title>
+    </ion-toolbar>
+    <ion-segment on:ionChange={(event) => section = event.detail.value }>
+        <ion-segment-button value="grades">
+            <ion-label>Grades</ion-label>
+        </ion-segment-button>
+        <ion-segment-button value="notes">
+            <ion-label>Notes</ion-label>
+        </ion-segment-button>
+    </ion-segment>
+</ion-header>
 
-	<ion-card-content>
-		Thank you for using this starter. Click buttons below to open these guides (will
-		open in new window). Don't forget to open DevTools to see this app in mobile mode. Happy coding!!!
-	</ion-card-content>
 
-	<ion-item>
-		<ion-label>Visit Ionic Showcase app with sourceviewer</ion-label>
-		<ion-button href="https://ionic-svelte.firebaseapp.com/" target="_new" fill="outline" slot="end"
-			>View</ion-button
-		>
-	</ion-item>
+<ion-content>
+    {#if section == "grades"}
+        <ion-card>
+            <p>Here are my grades.</p>
+            <ion-button on:click={() => getGrades()}>Load my courses</ion-button>
+        </ion-card>
+        
+        {#if courses.length == 0}
+            <ion-card>No courses found.</ion-card>
+        {:else}
+            <ion-list>
+                {#each courses as course}
+                <ion-item>
+                    <ion-label>Titlos: {course.courseTitle}</ion-label>
+                    {#if course.formattedGrade }
+                    <ion-text>Bathmos: {course.formattedGrade}</ion-text>
+                    {:else}
+                    <ion-text>Bathmos: No grade yet</ion-text>
+                    {/if}
+                </ion-item>
+                {/each}
+            </ion-list>
+        {/if}
+    
+    
+    
+    
+    
+    
+    {:else if section == "notes"}
+    <ion-card>
+        Here are my notes.
+    </ion-card>
+    {/if}
+</ion-content>
 
-	<ion-item>
-		<ion-label>Visit Ionic component docs</ion-label>
-		<ion-button
-			href="https://ionicframework.com/docs/components"
-			target="_new"
-			fill="outline"
-			slot="end">View</ion-button
-		>
-	</ion-item>
-	<ion-item>
-		<ion-label>Visit Svelte Kit docs</ion-label>
-		<ion-button
-			href="https://kit.svelte.dev/docs/introduction"
-			target="_new"
-			fill="outline"
-			slot="end">View</ion-button
-		>
-	</ion-item>
-	<ion-item>
-		<ion-label>Visit Svelte docs</ion-label>
-		<ion-button href="https://svelte.dev/docs" target="_new" fill="outline" slot="end"
-			>View</ion-button
-		>
-	</ion-item>
-</ion-card>
-
+<ion-footer>
+<ion-toolbar>
+    <ion-label>This is a footer!</ion-label>
+</ion-toolbar>
+</ion-footer>
   
